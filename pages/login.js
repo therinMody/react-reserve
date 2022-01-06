1b9dc6fa48b7e8a87e0ca61778e3e1f78b2a22c4
therinMody <therinharmenraymody@gmail.com>
2022-01-06 14:40:58 -0700

    Modeled User, Users can signup. Validation of POST content on Server. Added Login functionality

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Button, Form, Icon, Message, Segment } from 'semantic-ui-react';
 import Link from 'next/link';
-import catchErrors from '../utils/catchErrors'
+import catchErrors from '../utils/catchErrors';
+import axios from 'axios';
+import baseUrl from '../utils/baseUrl';
+import {handleLogin} from '../utils/auth';
 
 const INITIAL_USER = {
   email: "",
@@ -29,8 +32,10 @@ function Login() {
     try {
       setLoading(true);
       setError('');
-      console.log(user);
-      //make request to signup user
+      const url = `${baseUrl}/api/login`;
+      const payload = { ...user};
+      const response = await axios.post(url, payload);
+      handleLogin(response.data);
     } catch (error) {
       catchErrors(error, setError);
     } finally {
