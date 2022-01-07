c62ec5420545e72f197e41101ee8939c46c80617
therinMody <therinharmenraymody@gmail.com>
2022-01-07 15:38:35 -0700

    Added pagination for Product List. Created account header and account order history

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,28 @@
-import React from 'react'
-import axios from 'axios'
-import ProductList from '../components/Index/ProductList'
-import baseUrl from '../utils/baseUrl'
+import React from 'react';
+import axios from 'axios';
+import ProductList from '../components/Index/ProductList';
+import baseUrl from '../utils/baseUrl';
+import ProductPagination from '../components/Index/ProductPagination';
 
 //the home function
-function Home({products}) {
-  return <ProductList products={products}/>;
+function Home({products, totalPages}) {
+  return (
+    <>
+    <ProductList products={products}/>
+    <ProductPagination totalPages={totalPages}/>
+    </>
+  );
 }
 
-Home.getInitialProps = async () => {
-  //fetch data on server
+Home.getInitialProps = async ctx => {
+  const page = ctx.query.page ? ctx.query.page : "1";
+  const size = 9;
   const url = `${baseUrl}/api/products`;
-  const response = await axios.get(url);
-  return {products: response.data};
+  const payload = { params: {page, size }};
+  //fetch data on server
+  const response = await axios.get(url, payload);
   //return response data as an object
+  return response.data;
   // note: this object will be merged with existing props
 }
 
